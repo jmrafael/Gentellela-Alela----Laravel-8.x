26964874aa6d5e5285aac69c52a4b63a1f85a63c
Júlio Rafael <33831855+jmrafael@users.noreply.github.com>
2021-02-02 22:15:37 +0200

    PT Lang config

---
+++ resources/lang/pt/auth.php
@@ -0,0 +1,19 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Authentication Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are used during authentication for various
+    | messages that we need to display to the user. You are free to modify
+    | these language lines according to your application's requirements.
+    |
+    */
+
+    'failed' => 'Credencias de acesso inválidas.',
+    'throttle' => 'Muitas tentativas de entrada. Por favor, tente em :seconds segundos.',
+
+];
